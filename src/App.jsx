1a371c41fec055f1631c9c6766665ea3515c4863
Roniel <ronieldaboinchile@gmail.com>
2025-10-05 18:31:30 -0300

    RRSSUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HeaderEx } from "./components/header/Navbar/NavBar";
 import { Hero } from "./components/hero/hero";
 import { InfoSecction } from "./components/InfoSecction/InfoSecction";
+import { InstagramButton } from "./components/InfoSecction/support/SupportSectionStyle";
 
 function App() {
   return (
@@ -12,10 +13,17 @@ function App() {
         href="https://wa.me/12127365000?text=Hola,%20quiero%20más%20info!"
         target="_blank"
         rel="noopener noreferrer"
-        className="whatsapp-button"
-      >
-        WhatsApp
-      </a>
+        className="whatsapp-button"/>
+      <a
+        href="https://wa.me/12127365000?text=Hola,%20quiero%20más%20info!"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="Instagram-button"/>
+      <a
+        href="https://wa.me/12127365000?text=Hola,%20quiero%20más%20info!"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="Discord-button"/>
     </>
   );
 }
--- src/components/InfoSecction/support/SupportSection.jsx
+++ src/components/InfoSecction/support/SupportSection.jsx
@@ -1,29 +1,87 @@
-
-import { Container, Left, Right, ImageBuy, HeaderMain, Title, Content, Subtitle } from './SupportSectionStyle';
-
-
+import {
+  Container,
+  Left,
+  Right,
+  ImageBuy,
+  HeaderMain,
+  Title,
+  Content,
+  Subtitle,
+  InstagramButton,
+  WhatsappButton,
+  DiscordButton,
+  RRSS,
+} from "./SupportSectionStyle";
 
 export const SupportSection = () => {
   return (
     <>
-          <Container>
-            <Left>
-              <ImageBuy src="https://res.cloudinary.com/dra09b3f9/image/upload/v1759619805/pixelcut-export_-_2025-10-04T201639.332_gvainu.png" alt="buy section" />
-            </Left>
-            <Right>
-            <HeaderMain>Soporte y Atención al Cliente</HeaderMain>
-            <Title>Siempre disponibles para ayudarte</Title>
-            <Subtitle> En Games Hub, tu tranquilidad es lo más importante. </Subtitle>
-            <Content>Nuestro equipo de soporte está disponible para resolver tus dudas, guiarte durante el proceso de compra / venta y asegurarte una experiencia rápida y sin complicaciones.</Content>
-            <Title>¿Necesitas ayuda?</Title>
-            <Content> <strong> – Estados de pago o transferencias.</strong></Content>
-            <Content> <strong> – Detalles de compra de oro o saldo Blizzard.</strong></Content>
-            <Content> <strong>– Verificación de cuentas y métodos de pago.</strong></Content>
-            <Content> <strong>– Soporte técnico y asistencia personalizada.</strong></Content>
-            <Subtitle> Estamos aquí para ayudarte en todo momento. </Subtitle>
-            <Subtitle> Ya sea que necesites confirmar una transacción o resolver un detalle, nuestro equipo te responderá con la rapidez y seguridad que mereces. </Subtitle>
-            </Right>
-          </Container> 
-        </>
-  )
-}
+      <Container>
+        <Left>
+          {/* <ImageBuy src="https://res.cloudinary.com/dra09b3f9/image/upload/v1759637983/ChatGPT_Image_5_oct_2025_01_15_29_vf8c28.png" alt="buy section" /> */}
+          {/* <ImageBuy src="" alt="buy section" /> */}
+        </Left>
+        <Right>
+          <HeaderMain>Soporte y Atención al Cliente</HeaderMain>
+          <Title>Siempre disponibles para ayudarte</Title>
+          <Subtitle>
+            {" "}
+            En Games Hub, tu tranquilidad es lo más importante.{" "}
+          </Subtitle>
+          <Content>
+            Nuestro equipo de soporte está disponible para resolver tus dudas,
+            guiarte durante el proceso de compra / venta y asegurarte una
+            experiencia rápida y sin complicaciones.
+          </Content>
+          <Title>¿Necesitas ayuda?</Title>
+          <Content>
+            {" "}
+            <strong> – Estados de pago o transferencias.</strong>
+          </Content>
+          <Content>
+            {" "}
+            <strong> – Detalles de compra de oro o saldo Blizzard.</strong>
+          </Content>
+          <Content>
+            {" "}
+            <strong>– Verificación de cuentas y métodos de pago.</strong>
+          </Content>
+          <Content>
+            {" "}
+            <strong>– Soporte técnico y asistencia personalizada.</strong>
+          </Content>
+          <Subtitle> Estamos aquí para ayudarte en todo momento. </Subtitle>
+          <Subtitle>
+            {" "}
+            Ya sea que necesites confirmar una transacción o resolver un
+            detalle, nuestro equipo te responderá con la rapidez y seguridad que
+            mereces.{" "}
+          </Subtitle>
+        <RRSS>
+          <InstagramButton
+            href="https://www.instagram.com/gameshub.oficial/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </InstagramButton>
+          <WhatsappButton
+            href="https://wa.me/573053606634"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            WhatsApp
+          </WhatsappButton>
+          <DiscordButton
+            href="https://discord.gg/4ZtHhYvG"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Discord
+          </DiscordButton>
+        </RRSS>
+        </Right>
+      </Container>
+    </>
+  );
+};
